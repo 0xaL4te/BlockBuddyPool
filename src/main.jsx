cff5455fa03ae6deb7847eb594eea22ee574b7ab
0xaL4te <manomithran03@gmail.com>
2023-05-05 16:32:45 +0530

    updating

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import Aave from './aave'
+import Notional from './notional'
+import Title from './title'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Title />
+  </React.StrictMode>,
+)
